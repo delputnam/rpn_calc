a6c63e822a1a56b6dc4de61cfa665830606e4585
Del Putnam <del@putnams.net>
2016-05-15 17:06:18 -0400

    Added test for invalid multi-byte strings as operators

--- rpn_calc_PerformOperation_test.go
+++ rpn_calc_PerformOperation_test.go
@@ -105,4 +105,29 @@ func TestPerformOperationFail(t *testing.T) {
 			t.Errorf("Expected error when using invalid operator, Got nil.")
 		}
 	}
+
+	// Test some random strings of random length greater than 2
+	// since all multi-character strings are invalid.
+	aLen := len(ASCII)
+	for tn := 0; tn < 1000; tn++ {
+		len := rand.Intn(100) + 2 // min length of 2 chars
+		var tc string
+		for i := 0; i < len; i++ {
+			aRand := rand.Intn(aLen)
+			tc += string(ASCII[aRand])
+		}
+		operand1 := (rnd.Float64() * float64(rnd.Intn(10000))) - float64(rnd.Intn(10000))
+		operand2 := (rnd.Float64() * float64(rnd.Intn(10000))) - float64(rnd.Intn(10000))
+
+		expectedResult := float64(0)
+		gotResult, gotErr := PerformOperation(operand1, operand2, tc)
+
+		if expectedResult != gotResult {
+			t.Errorf("Expected result of 0 when using invalid operator, Got: %v", gotResult)
+		}
+
+		if gotErr == nil {
+			t.Errorf("Expected error when using invalid operator, Got nil.")
+		}
+	}
 }
