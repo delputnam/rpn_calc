e0d5e34bfd90aca352ba6d3e69f371de3445b09f
Del Putnam <del@putnams.net>
2016-05-15 09:18:57 -0400

    Initial commit of source file.

---
+++ rpn_calc.go
@@ -0,0 +1,29 @@
+// RPN Calculator
+
+// open Stdin reader
+
+// begin loop
+
+  // prompt for input
+
+  // read until new line
+
+  // exit on EOF
+
+  // convert read bytes to string
+
+  // push onto stack if valid operand
+
+  // perform op if valid operator
+
+    // Exit if "q"
+
+    // check for at least 2 operands in stack to continue
+
+    // get result
+
+    // pop operands off stack
+
+    // push result onto stack
+
+  // display output
